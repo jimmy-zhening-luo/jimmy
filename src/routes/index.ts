526032e0e04150000778caa64c28b9f7f31d5f90
Jimmy Zhening Luo <zluo@live.com>
2022-09-01 20:17:13 -0700

    Update tsconfig to enable stricter type checking.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,15 @@
 import * as express from "express";
 
 export const register = (app: express.Application) => {
-    app.get("/", (req: any, res) => {
+    app.get("/", (_req: any, res) => {
         res.render("index");
     });
 
-    app.get("/debug", (req: any, res) => {
+    app.get("/debug", (_req: any, res) => {
         res.render("debug");
     });
 
-    app.get("/purposeful-bork", (req: any, res) => {
+    app.get("/purposeful-bork", (_req: any, res) => {
         res.render("purposeful-bork");
     });
 };
