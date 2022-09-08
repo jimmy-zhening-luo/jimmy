60deee803edd3d88d52aece5ff3b8f2f3df655eb
Jimmy Zhening Luo <zluo@live.com>
2022-09-07 20:09:28 -0700

    renaming ./src/index.ts to server.ts to avoid ambiguity and make clear that server.ts is the Express server app and that client.ts is the Svelte client app.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+// import cors from "cors";
 import path from "path";
 import * as routes from "./routes";
 
@@ -9,6 +10,8 @@ const host: string = <string>(process.env["HOST"]) ?? <string>(process.env["LOCA
 const port: string = <string>(process.env["PORT"]) ?? <string>(process.env["LOCAL_TEST_PORT"]);
 
 const app: express.Application = express();
+
+app.use(express.static('./'))
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
