a8572d18921b48da703721fc6bab58811cf55801
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2023-12-12 19:07:34 -0800

    Pulling footer out of layout doc for readability / consistency with Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
   import Header from "./Header.svelte";
+  import Footer from "./Footer.svelte";
   import "./styles.css";
 </script>
 
@@ -10,22 +11,7 @@
     <slot />
   </main>
 
-  <footer>
-    <span class="copyright">
-      <p>
-        &copy; 2023 Jimmy Zhening Luo
-      </p>
-    </span>
-    <span class="view-source">
-      <p>
-        <code>
-          <a href="https://github.com/jimmy-zhening-luo/jimmy">
-            View Source
-          </a>
-        </code>
-      </p>
-    </span>
-  </footer>
+  <Footer />
 </div>
 
 <style>
@@ -44,28 +30,5 @@
     padding: 1rem;
     width: 100%;
     max-width: 64rem;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 1rem;
-    padding: 12px 12px 85px 12px;
-  }
-
-  footer p {
-    font-size: 0.8rem;
-  }
-
-  footer .copyright p {
-    font-weight: 600;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0px 85px 0px;
-    }
   }
 </style>
