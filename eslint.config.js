d582ceb892f7eacef1f43caab2d01b59dd99a942
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-03 01:35:13 -0700

    npm reinstall; consistent eslint config import names

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import stylisticPlugin from "@stylistic/eslint-plugin";
-import tsLintPlugin from "@typescript-eslint/eslint-plugin";
-import tsLintParser from "@typescript-eslint/parser";
+import stylistic from "@stylistic/eslint-plugin";
+import ts from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import Configs from "@jimbojet/lint";
 
 export default [
   ...new Configs(
-    stylisticPlugin,
-    tsLintPlugin,
-    tsLintParser,
+    stylistic,
+    ts,
+    tsParser,
     [
       "svelte.config.js",
       "eslint.config.js",
