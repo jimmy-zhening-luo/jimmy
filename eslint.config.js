680e2744ac5641a25e07d158c85e0ec6e5c2225e
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-05-03 22:47:49 -0700

    linter v8 beta

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,45 @@
 import stylistic from "@stylistic/eslint-plugin";
 import ts from "@typescript-eslint/eslint-plugin";
+import svelte from "eslint-plugin-svelte";
 import tsParser from "@typescript-eslint/parser";
+import svelteParser from "svelte-eslint-parser";
 import Configs from "@jimbojet/lint";
 
 export default [
   ...new Configs(
-    stylistic,
-    ts,
-    tsParser,
-    [
-      "svelte.config.js",
-      "eslint.config.js",
-    ],
-    [
-      "vite.config.ts",
-      "src/**/*.ts",
-    ],
-    undefined,
+    {
+      stylistic,
+      files: [
+        "svelte.config.js",
+        "eslint.config.js",
+      ],
+    },
+    {
+      ts,
+      parser: tsParser,
+      files: [
+        "vite.config.ts",
+        "src/**/*.ts",
+      ],
+    },
+    {
+      overrideSvelte: {
+        "svelte/block-lang": [
+          "error",
+          {
+            enforceScriptPresent: true,
+            enforceStylePresent: false,
+            script: ["ts"],
+          },
+        ],
+        "@typescript-eslint/no-unused-vars": "off",
+      },
+    },
+    {
+      svelte,
+      parser: svelteParser,
+      processor: "svelte/svelte",
+      files: ["src/**/*.svelte"],
+    },
   ).configs,
 ];
