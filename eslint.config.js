0a6814a92774215917a7d1e6149b2c1bc91fedf2
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-01 22:07:13 -0700

    bug: update eslint config to new lint api

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
-import stylistic from "@stylistic/eslint-plugin";
 import svelte from "eslint-plugin-svelte";
 import svelteParser from "svelte-eslint-parser";
 import Lint from "@jimbojet/lint";
 
 export default [
   ...new Lint(
-    stylistic,
     {
       js: ["*.config.js"],
       ts: [
