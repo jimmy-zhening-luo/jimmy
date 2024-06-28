10903066324ca575ff85fd6f9508afc814bfb700
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-06-27 18:47:35 -0700

    upgrade package.json template v20, update eslint.config v12.1

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,21 @@
+// eslint.config.js
+// v12.1.2
 import linted from "linted";
 
 export default [
   ...new linted(
     {
       js: [
-        "eslint.config.js",
         "svelte.config.js",
+        "eslint.config.js",
       ],
       ts: [
         "vite.config.ts",
         "src/**/*.ts",
       ],
       svelte: ["src/**/*.svelte"],
-      jest: ["src/**/*.spec.ts"],
       html: ["src/**/*.html"],
+      jest: ["src/**/*.spec.ts"],
       jsonc: ["tsconfig.json"],
       json: ["package.json"],
       yml: [".github/workflows/*.yml"],
