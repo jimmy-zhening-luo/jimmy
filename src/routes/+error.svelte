3c3e7f1729713df9a4c0b08144706e1787f5e5fe
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2024-09-04 12:51:24 -0700

    site: +custom 404 page content

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script
+  lang="ts"
+  strictEvents>
+</script>
+
+<svelte:head>
+  <title>Jimmy</title>
+  <meta
+    name="description"
+    content="Jimmy Luo's personal website" />
+</svelte:head>
+
+<section>
+  <h1>
+    404
+  </h1>
+  <code>
+    Page Not Found
+  </code>
+</section>
