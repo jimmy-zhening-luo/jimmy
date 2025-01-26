6fb9b20c5eaf7bb3abf341a6231ac86b151a939e
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2025-01-26 13:59:31 -0800

    Social: +apps manifest

---
+++ src/routes/_social/apps.ts
@@ -0,0 +1,52 @@
+export default {
+  facebook: {
+    name: "Facebook",
+    host: "www.facebook.com",
+    username: "jimbojet",
+    post: "/",
+  },
+  instagram: {
+    name: "Instagram",
+    host: "www.instagram.com",
+    username: "_jimbojet",
+    post: "/",
+  },
+  substack: {
+    name: "Substack",
+    host: "read.jimm.my",
+  },
+  github: {
+    name: "GitHub",
+    host: "github.com",
+    username: "jimmy-zhening-luo",
+  },
+  youtube: {
+    name: "YouTube",
+    host: "www.youtube.com",
+    username: "@jimbojet",
+  },
+  spotify: {
+    name: "Spotify",
+    host: "open.spotify.com",
+    username: "1215989421",
+    pre: "user/",
+  },
+  soundcloud: {
+    name: "SoundCloud",
+    host: "soundcloud.com",
+    username: "jimmyluo",
+  },
+  duolingo: {
+    name: "Duolingo",
+    host: "www.duolingo.com",
+    username: "jimbojet",
+    pre: "profile/",
+  },
+  linkedin: {
+    name: "LinkedIn",
+    host: "www.linkedin.com",
+    username: "jimmy-zhening-luo",
+    pre: "in/",
+    post: "/",
+  },
+} as const;
