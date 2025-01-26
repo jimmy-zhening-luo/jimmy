c105c9afeb2454be68b4a603011ffa0c9c35f13c
Jimmy Zhening Luo <1450044+jimmy-zhening-luo@users.noreply.github.com>
2025-01-26 15:00:20 -0800

    contract manifest

--- src/routes/_social/apps.ts
+++ src/routes/_social/apps.ts
@@ -1,52 +1,11 @@
 export default {
-  facebook: {
-    name: "Facebook",
-    host: "www.facebook.com",
-    username: "jimbojet",
-    post: "/",
-  },
-  instagram: {
-    name: "Instagram",
-    host: "www.instagram.com",
-    username: "_jimbojet",
-    post: "/",
-  },
-  substack: {
-    name: "Substack",
-    host: "read.jimm.my",
-  },
-  github: {
-    name: "GitHub",
-    host: "github.com",
-    username: "jimmy-zhening-luo",
-  },
-  youtube: {
-    name: "YouTube",
-    host: "www.youtube.com",
-    username: "@jimbojet",
-  },
-  spotify: {
-    name: "Spotify",
-    host: "open.spotify.com",
-    username: "1215989421",
-    pre: "user/",
-  },
-  soundcloud: {
-    name: "SoundCloud",
-    host: "soundcloud.com",
-    username: "jimmyluo",
-  },
-  duolingo: {
-    name: "Duolingo",
-    host: "www.duolingo.com",
-    username: "jimbojet",
-    pre: "profile/",
-  },
-  linkedin: {
-    name: "LinkedIn",
-    host: "www.linkedin.com",
-    username: "jimmy-zhening-luo",
-    pre: "in/",
-    post: "/",
-  },
+  Duolingo: { host: "www.duolingo.com", username: "jimbojet", path: { pre: "profile/" } },
+  Facebook: { host: "www.facebook.com", username: "jimbojet", path: { post: "/" } },
+  GitHub: { host: "github.com", username: "jimmy-zhening-luo" },
+  Instagram: { host: "www.instagram.com", username: "_jimbojet", path: { post: "/" } },
+  LinkedIn: { host: "www.linkedin.com", username: "jimmy-zhening-luo", path: { pre: "in/", post: "/" } },
+  SoundCloud: { host: "soundcloud.com", username: "jimmyluo" },
+  Spotify: { host: "open.spotify.com", username: "1215989421", path: { pre: "user/" } },
+  Substack: { host: "read.jimm.my" },
+  YouTube: { host: "www.youtube.com", username: "@jimbojet" },
 } as const;
