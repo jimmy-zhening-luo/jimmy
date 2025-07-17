ff465566609f838a0d2e292b3f5a4c6a336ff488
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-07-17 14:46:59 -0700

    test client-side load

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,20 @@
+<script
+  lang="ts"
+>
+  const { data } = $props(),
+  { content } = data;
+</script>
+
+<svelte:head>
+  <title>Jimmy: Test</title>
+  <meta
+    name="description"
+    content="Jimmy Luo's personal website: a test page"
+  />
+</svelte:head>
+
+<section>
+  <h1>
+    {content}
+  </h1>
+</section>
