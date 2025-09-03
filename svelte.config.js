ea5d3bf0634e2fb0879e8207a5c6e6ef1a0d0181
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-09-03 15:14:22 -0700

    svelte.config.js: prod: -typeguard to direct export

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-static";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   preprocess: vitePreprocess(),
   kit: { adapter: adapter({}) },
 };
-
-export default config;
