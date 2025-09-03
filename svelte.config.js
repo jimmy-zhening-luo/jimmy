0d5141df28cef57d67327cfaf974db5abd78ec50
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2025-09-03 15:35:43 -0700

    svelte.config.js: add back type annotation now that it has good type

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import adapter from "@sveltejs/adapter-static";
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   preprocess: vitePreprocess(),
   kit: {
